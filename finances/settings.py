faec5a0a6cb20bd9a7a7e87d8f1a0bb2a2ca9bab
Ceasar15 <ceasarkwadwo@gmail.com>
2020-10-12 05:30:14 +0100

    database bug fixed

--- finances/settings.py
+++ finances/settings.py
@@ -3,7 +3,7 @@ import os
 import django_heroku
 import dj_database_url
 import dotenv
-
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,18 +78,19 @@ WSGI_APPLICATION = 'finances.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
-        #'NAME': BASE_DIR/'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
+#         #'NAME': BASE_DIR/'db.sqlite3',
         
-    }
-}
+#     }
+# }
 
 # Heroku Database Config
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES={'default' : dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
@@ -160,5 +161,8 @@ SITE_ID = 1
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-#options = DATABASES['default'].get('OPTIONS', {})
-#options.pop('sslmode', None)
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmode', None)
+
+
+#DATABASE_URL = os.environ['DATABASE_URL'] 
