2a9fb6b2b84b9d67165a09e625f1293fe89a78c7
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-04 15:30:07 +0000

    adey come

--- finances/settings.py
+++ finances/settings.py
@@ -8,7 +8,6 @@ import dotenv
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'youth/static/js', 'serviceworker.js')
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
@@ -211,4 +210,39 @@ PAYSTACK_SECRET_KEY='sk_live_be9332d2f408b2362c8afbbb3213c4d988b39312'
 # )
 
 # Cache Settings
+
+# PWA Settings
+
+
+#PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'youth/static/js', 'serviceworker.js')
+
+PWA_APP_NAME = 'Shalom 1'
+PWA_APP_DESCRIPTION = "GEC Youth Church"
+PWA_APP_THEME_COLOR = '#0A0302'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/images/cross.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/images/cross.png',
+        'sizes': '160x160'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/images/cross.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
 
